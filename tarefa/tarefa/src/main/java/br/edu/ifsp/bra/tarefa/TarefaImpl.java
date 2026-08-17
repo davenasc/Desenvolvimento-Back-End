fae9d0de3dc81671a02cbbd8b6d979e119ff9681
David Nascimento Martins <powdx@live.com.pt>
2026-08-17 19:39:00 -0300

    exercicio aula 1

--- tarefa/tarefa/src/main/java/br/edu/ifsp/bra/tarefa/TarefaImpl.java
+++ tarefa/tarefa/src/main/java/br/edu/ifsp/bra/tarefa/TarefaImpl.java
@@ -1,12 +1,14 @@
 package br.edu.ifsp.bra.tarefa;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+//mesmo alterando para service a saída continua a mesma.
+//após alterar para o AppConfig e usar a notation @configuration, continua funcionando.
 public class TarefaImpl implements Tarefa {
 
     @Override
     public void executar() {
-        System.out.println("Executando uma tarefa...");
-    }    
+        System.out.println("Executando tarefa...");
+    }
 }
